1e73b41c935d9df30eab53b7bccec5ca08fda2b2
Komalpreet Singh <komalbhatia2305@gmail.com>
2025-05-03 17:54:20 -0400

    update in models

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -1,3 +1,100 @@
-//resetPasswordToken
+const User = require("../models/User");
+const mailSender = require("../utils/mailSender");
+const bcrypt = require("bcrypt");
 
+//resetPasswordToken
+exports.resetPasswordToken = async (req, res) => {
+  try {
+    //get email from req body
+    const email = req.body.email;
+    //check user for this email
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Email is not registered",
+      });
+    }
+    //if email is ok, generate token using crypto
+    const token = crypto.randomUUID();
+    //update user by adding token and expiration time
+    const updatedDetails = await User.findOneAndUpdate(
+      { email: email },
+      {
+        token: token,
+        resetPasswordExpires: Date.now() + 5 * 60 * 1000,
+      },
+      { new: true }
+    );
+    //create url
+    const url = `http://localhost:3000/update-password/${token}`;
+    //send mail containing url, and return response
+    await mailSender(
+      email,
+      "Passoword Reset Instructions",
+      `Password reset link: ${url}`
+    );
+    return res.status(200).json({
+      success: true,
+      message: "check email and reset password",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong, try again later",
+    });
+  }
+};
 //resetPassword
+
+exports.resetPassword = async (req, res) => {
+  try {
+    //get user data from body
+    //note: in frontend we put our data in the req to send at backend
+    const { token, password, confirmPassword } = req.body;
+    //validation
+    if (password !== confirmPassword) {
+      return res.status(401).json({
+        success: false,
+        message: "Password do not match",
+      });
+    }
+    //get user details from db using token
+    const userDetails = await User.findOne({ token: token });
+    //if no entry -  invalid token
+    if (!userDetails) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is invalid",
+      });
+    }
+    //token time check
+    if (userDetails.resetPasswordExpires < Date.now()) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is expired, please regenerate your token",
+      });
+    }
+    //password hash
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //update in db
+    await User.findOneAndUpdate(
+      { token: token },
+      { password: hashedPassword },
+      { new: true }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "password updated successfully",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "something went wrong",
+    });
+  }
+};
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,7 +1,99 @@
-//auth
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../models/User");
 
+//auth
+exports.auth = async (req, res, next) => {
+  try {
+    //extract token
+    const token =
+      req.body.token ||
+      req.cookies.token ||
+      req.header("Authorization").replace("bearer ", "");
+
+    //check if token is present
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Missing token",
+      });
+    }
+
+    //verify the token
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decode);
+
+      req.user = decode;
+    } catch (e) {
+      return res.status(401).json({
+        success: false,
+        message: "token is invalid",
+      });
+    }
+    next();
+  } catch (e) {
+    console.log(e);
+    return res.status(401).josn({
+      success: false,
+      message: "Someting went wrong while validatijng the token",
+    });
+  }
+};
 //isStudent
 
+exports.isStudent = async (req, res, next) => {
+  try {
+    console.log("test this request how the user data is coming " + req);
+    if (req.user.accountType !== "Student") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a protected route for students only",
+      });
+    }
+    next();
+  } catch (e) {
+    return res.status(500).json({
+      success: false,
+      message: "User account type cannot be verified",
+    });
+  }
+};
+
 //isInstructor
+exports.isInstructor = async (req, res, next) => {
+  try {
+    console.log("test this request how the user data is coming " + req);
+    if (req.user.accountType !== "Instructor") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a protected route for instructors only",
+      });
+    }
+    next();
+  } catch (e) {
+    return res.status(500).json({
+      success: false,
+      message: "User account type cannot be verified",
+    });
+  }
+};
 
 //isAdmin
+exports.isAdmin = async (req, res, next) => {
+  try {
+    console.log("test this request how the user data is coming " + req);
+    if (req.user.accountType !== "Admin") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a protected route for admin only",
+      });
+    }
+    next();
+  } catch (e) {
+    return res.status(500).json({
+      success: false,
+      message: "User account type cannot be verified",
+    });
+  }
+};
